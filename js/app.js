ba9c6915c3ef95682390934ae560ba7e09921865
smillyviral-bit <smillyviral@gmail.com>
2026-09-03 00:18:01 +0100

    Implement preloader hide after loading
    Add preloader functionality on window load

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+window.addEventListener("load", () => {
 
+    const preloader = document.getElementById("preloader");
+
+    setTimeout(() => {
+
+        preloader.classList.add("preloader-hide");
+
+    }, 1800);
+
+});
